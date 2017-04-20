7d0832e356bd980f69be97b703ebe6cafdc148b3
vlad1777d <naumovvladislav@mail.ru>
2017-04-20 03:00:06 +0300

    Added language switcher.

--- pelican_sources/sources/theme_general/static/js/sticky_panel.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('#top_panel').addClass("sticky");
-    }
-    else{
-        $('#top_panel').removeClass("sticky");
-    }
-});
