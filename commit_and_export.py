1b5680e2140495d0200500f40d264a65c0dcbad3
vlad1777d <naumovvladislav@mail.ru>
2017-07-12 23:52:09 +0300

    Corrected export script.

--- commit_and_export.py
+++ commit_and_export.py
@@ -6,4 +6,6 @@ import os
 os.system('hg addremove')
 os.system('hg commit')
 os.system('hg push git+ssh://git@github.com:vlad1777d/vlad1777d.github.io.git')
+# if there's error: ""
+# then you can make "hg gc" and then repeat pushing attempt
 print("Script finished.")
