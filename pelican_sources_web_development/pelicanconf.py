e7181107d797df5aa20bb4540d137109422ea23b
vlad1777d <naumovvladislav@mail.ru>
2017-07-12 23:44:31 +0300

    Reordered project structure. Seprated some pages to different pelican source folders. Removed Bootstrap 3 and JQuery dependencies from page_web_development.html page.

---
+++ pelican_sources_web_development/pelicanconf.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+
+from __future__ import unicode_literals
+import os.path
+join = os.path.join
+import sys
+
+
+
+
+AUTHOR = u'Vladislav'
+SITENAME = u'Разработка веб-сайтов.'
+SITEURL = ''
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # сделал по образу и подобию Django
+PATH = 'sources'
+STATIC_PATHS = []  # будут внутри папки PATH
+ARTICLE_PATHS = []
+THEME = join(PATH, 'theme_general')
+DIRECT_TEMPLATES = ['page_web_development']
+# ^ здесь написать имена тех шаблонов, которые будут включены в постоение
+OUTPUT_PATH = '../page_web_development'
+DELETE_OUTPUT_DIRECTORY = True
+
+
+TIMEZONE = 'Europe/Kiev'
+DEFAULT_LANG = u'ru'
+
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+
+JINJA_ENVIRONMENT = {}
+# may be {'line_statement_prefix': '#', 'line_comment_prefix': '##'}
+
+
+PLUGIN_PATHS = [join(PATH, 'plugins')]
+PLUGINS = ['i18n_subsites']
+I18N_SUBSITES = {
+    'en': {
+        'SITENAME': 'Web development',
+        'DL': 'en'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
